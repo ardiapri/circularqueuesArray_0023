f360bae0765014822e70979960a7493286145e39
ardiapri <ardiapri882@gmail.com>
2024-05-30 18:24:58 +0700

    int from dan int rear

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -59,5 +59,15 @@ public:
     }
 
     void display() {
-        int FRONT_position = FRONT
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        //cek apakah antrian kosong 
+        if (FRONT == -1) {
+            cout << "queue is empty\n";
+            return;
+        }
+
+        
     }
+};
