04b8bab3136ff81b7769fbb4755cc1edf2fdccc9
ardiapri <ardiapri882@gmail.com>
2024-05-30 17:38:33 +0700

    class queues

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+
+public:
+   queues() {
+      FRONT = -1;
+      REAR = -1;
+   }
+
+   
